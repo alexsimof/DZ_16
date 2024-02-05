2b7f5cf01ee48af054eadbd7dbd478a5af28a9f7
Alex Simonov <alex.simonof@gmail.com>
2024-02-05 12:58:59 +0200

    add header

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+let btn = document.querySelector(".header-menu-toggle");
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let activ = document.querySelector(".header-nav-mobile");
+  
+  activ.classList.toggle('activ')
+
+});
